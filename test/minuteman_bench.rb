d60073c5643ecca83264e89ad2f21e323330894c
elcuervo <yo@brunoaguirre.com>
2015-10-17 16:37:28 +1300

    changes benchmarks to use IPS

--- test/minuteman_bench.rb
+++ test/minuteman_bench.rb
@@ -1,5 +1,5 @@
 require 'minuteman'
-require 'benchmark'
+require 'benchmark/ips'
 
 Minuteman.configure do |config|
   config.redis = Redic.new("redis://127.0.0.1:6379/1")
@@ -17,46 +17,56 @@ Minuteman.track("first:page", users_one)
 Minuteman.track("second:page", users_two)
 Minuteman.track("third:page", users_three)
 
-Benchmark.bmbm(7) do |x|
-  x.report("tracking users") {
-    Minuteman.track("first:page", users_three)
-  }
+Benchmark.ips do |x|
+  x.report("tracking users") { Minuteman.track("first:page", users_three) }
 
   x.report("tracking annoymous users") {
-    1000.times { Minuteman.track("first:page") }
+    Minuteman.track("first:page")
   }
 
   x.report("operation: AND") {
-    1000.times { Minuteman("first:page").day & Minuteman("second:page").day }
+    Minuteman("first:page").day & Minuteman("second:page").day
   }
 
   x.report("operation: OR") {
-    1000.times { Minuteman("first:page").day | Minuteman("second:page").day }
+    Minuteman("first:page").day | Minuteman("second:page").day
   }
 
   x.report("operation: XOR") {
-    1000.times { Minuteman("first:page").day ^ Minuteman("second:page").day }
+    Minuteman("first:page").day ^ Minuteman("second:page").day
   }
 
   x.report("operation: NOT") {
-    1000.times { -Minuteman("second:page").day }
+    -Minuteman("second:page").day
   }
 
   x.report("operation: MINUS") {
-    1000.times { Minuteman("first:page").day + Minuteman("second:page").day }
+    Minuteman("first:page").day + Minuteman("second:page").day
   }
 
   x.report("complex operations") {
-    1000.times {
-      (
-        Minuteman("first:page").day + Minuteman("second:page").day
-      ) - Minuteman("third:page").day
-    }
+    (
+      Minuteman("first:page").day + Minuteman("second:page").day
+    ) - Minuteman("third:page").day
   }
 
   x.report("adding to the counter") {
-    1000.times {
-      Minuteman.add("first:counter")
-    }
+    Minuteman.add("first:counter")
+  }
+
+  x.report("checking the counter") {
+    Counterman("first:counter").month.count
+  }
+
+  x.report("tracking through a user") {
+    users.sample.track("some:event")
+  }
+
+  x.report("counting through a user") {
+    users.sample.add("some:event")
+  }
+
+  x.report("checking the counter through a user") {
+    users.sample.count("some:event").day.count
   }
 end
